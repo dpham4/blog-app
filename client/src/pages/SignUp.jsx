4028ade443d02cff059c1b33eae76e072d6c918d
dpham4 <dpham4@binghamton.edu>
2024-02-27 16:55:32 +0700

    Start working on signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="min-h-screen mt-20">
+      <div className="">
+        <Link to="/" className="font-bolddark:text-white text-4xl">
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Alexa Blog Center</span>
+        </Link>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
