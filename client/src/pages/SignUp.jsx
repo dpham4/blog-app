852c580543ce82e090470a45bc38e6045f665123
dpham4 <dpham4@binghamton.edu>
2024-02-27 20:33:20 +0700

    Change the header to have padding

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -5,9 +5,10 @@ export default function SignUp() {
   return (
     <div className="min-h-screen mt-20">
       <div className="">
-        <Link to="/" className="font-bolddark:text-white text-4xl">
+        <Link to="/" className="font-bold dark:text-white text-4xl">
           <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Alexa Blog Center</span>
         </Link>
+        <p className="text-sm mt-5">This is a demo project. You can sign in with your email and password or with Google.</p>
       </div>
       <div>
 
