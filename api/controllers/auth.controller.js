b0b3bb6ca5534b5dd9d7133d68155a30a14b3ea5
dpham4 <dpham4@binghamton.edu>
2024-02-27 16:28:51 +0700

    Finish signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,22 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
 
 export const signUp = async (req, res) => {
+    console.log(req.body)
     const {username, email, password } = req.body;  
-    // console.log({username, email, password});
 
     if(!username || !email || !password || username === "" || email === "" || password === "") {
        res.status(400).json({message: "All fields are required"}); 
     }
 
-    const newUser = new User({username, email, password})
-    await newUser.save();
-    res.json("Sign up successfully") 
+    const hashedPassword = await bcryptjs.hashSync(password, 10)
+
+    try {
+        const newUser = new User({username, email, password: hashedPassword})
+        await newUser.save();
+        res.json('Sign up successfully') 
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+  
 } 
--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.json());
 
 
+
 const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
